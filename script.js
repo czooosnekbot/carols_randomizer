75c15b3000c19f2ae63855f6f418b568e5f675af
czooosnekplow <mrojek@czooosnek.cloud>
2019-12-28 21:11:07 +0100

    pre for last carol

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ var rollButton = document.querySelector("#roll")
 var carolsText = document.querySelectorAll(".carols-text")
 var startInfo = document.querySelector(".start-info")
 var navBar = document.querySelector(".nav")
-let carolRandomizer = function() {
+var lastCarolTitleBar = document.querySelector("#last")
+var latestValues = [];
+let carolRandomizer = (e) => {
     return Math.floor(Math.random() * (carolsText.length))
 }
-let carolsHidder = function() {
+let carolsHidder = (e) => {
     for (i = 0; i < carolsText.length; i++) {
         carolsText[i].style.display = "none";
     }
     startInfo.style.display = "none";
 }
-
-rollButton.addEventListener("click", (e) => {
-    carolsHidder()
-    let randomizedCarolNumber = carolRandomizer()
-    console.log("Numer wylosowanej kolędy: " + (randomizedCarolNumber + 1) + `, ID: ${randomizedCarolNumber}`)
-    document.getElementById(`${randomizedCarolNumber}`).style.display = "block";
-    navBar.className = "nav-sticked d-flex flex-column"
-})
+let carolChanger = (e) => {
+    rollButton.addEventListener("click", (e) => {
+        carolsHidder()
+        let randomizedCarolNumber = carolRandomizer()
+        console.log("Numer wylosowanej kolędy: " + (randomizedCarolNumber + 1) + `, ID: ${randomizedCarolNumber}`)
+        document.getElementById(`${randomizedCarolNumber}`).style.display = "block";
+        navBar.className = "nav-sticked d-flex flex-column"
+        document.changedCarol = randomizedCarolNumber;
+        document.latestValues = [];
+        document.latestValues.push(randomizedCarolNumber)
+        lastCarolShower()
+    })
+}
+carolChanger()
+let lastCarolShower = (e) => {
+    lastCarolTitleBar.innerHTML = document.querySelectorAll("h1")[document.latestValues[latestValues - 2] + 1].innerHTML
+}
