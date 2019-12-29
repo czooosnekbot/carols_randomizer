e112894cba7c127574617e4fda5a0165a311614a
czooosnekplow <mrojek@czooosnek.cloud>
2019-12-29 21:11:35 +0100

    name of prev randomized carol available in nav

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var rollButton = document.querySelector("#roll")
-var carolsText = document.querySelectorAll(".carols-text")
 var startInfo = document.querySelector(".start-info")
 var navBar = document.querySelector(".nav")
+var rollButton = document.querySelector("#roll")
+var carolsText = document.querySelectorAll(".carols-text")
+var lastCarolLabel = document.querySelector("#last-label")
 var lastCarolTitleBar = document.querySelector("#last")
 document.lastValues = new Array()
 let carolRandomizer = (e) => {
@@ -19,7 +20,7 @@ let carolChanger = (e) => {
         let randomizedCarolNumber = carolRandomizer()
         console.log("Numer wylosowanej kolędy: " + randomizedCarolNumber)
         document.getElementById(`${randomizedCarolNumber}`).style.display = "block";
-        navBar.className = "nav-sticked d-flex flex-column"
+        navBar.className = "nav-sticked d-flex flex-md-column"
         document.changedCarol = randomizedCarolNumber;
         document.lastValues.push(document.changedCarol)
         console.log("Historia losowania: " + document.lastValues)
@@ -29,5 +30,10 @@ let carolChanger = (e) => {
 }
 carolChanger()
 let latestCarolShower = (e) => {
-    document.querySelector("#last").innerHTML = document.querySelectorAll("h1")[document.lastValues[document.lastValues.length - 2]].innerHTML
+    if ((document.lastValues[document.lastValues.length - 2]) != undefined) {
+        lastCarolLabel.style.display = "block";
+        return document.querySelector("#last").innerHTML = document.querySelectorAll("h1")[(document.lastValues[document.lastValues.length - 2]) + 1].innerHTML
+    } else {
+        return document.querySelector("#last").innerHTML = ""
+    }
 }
