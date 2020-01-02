94b2c84d4a81598963a1637c643689595adfba1b
czooosnekplow <mrojek@czooosnek.cloud>
2020-01-02 22:19:47 +0100

    js script improvements
    Thanks to great guy Arkadiusz! :100:

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const startRollingFormCheckbox = document.querySelector("#WNCcheckbox")
 const carolsText = document.querySelectorAll(".carols-text")
 const lastCarolLabel = document.querySelector("#last-label")
 const lastCarolTitleBar = document.querySelector("#last")
-document.lastValues = new Array()
-let carolRandomizer = (e) => {
-    const lastCarolValue1 = document.lastValues[document.lastValues.length - 1]
-    const lastCarolValue2 = document.lastValues[document.lastValues.length - 2]
-    let random = Math.floor(Math.random() * (carolsText.length))
-    if (startRollingFormCheckbox.checked & document.lastValues.length === 0) {
+const lastValues = []
+const carolRandomizer = () => {
+    const lastCarolValue1 = lastValues[lastValues.length - 1]
+    const lastCarolValue2 = lastValues[lastValues.length - 2]
+    const random = Math.floor(Math.random() * (carolsText.length))
+    if (startRollingFormCheckbox.checked & lastValues.length === 0) {
         return 0
     } else if (random != lastCarolValue1 & random != lastCarolValue2) {
         return random
@@ -18,31 +18,33 @@ let carolRandomizer = (e) => {
         return carolRandomizer()
     }
 }
-let carolsHidder = (e) => {
+const carolsHidder = () => {
     for (i = 0; i < carolsText.length; i++) {
         carolsText[i].style.display = "none";
     }
     startInfo.style.display = "none";
 }
-let carolChanger = (e) => {
+const latestCarolShower = () => {
+    if ((lastValues[lastValues.length - 2]) != undefined) {
+        lastCarolLabel.style.display = "block";
+        document.querySelector("#last").innerHTML = document.querySelectorAll("h1")[(lastValues[lastValues.length - 2]) + 1].innerHTML
+    } else {
+        document.querySelector("#last").innerHTML = ""
+    }
+}
+const navMobileSticker = () => {
+    navBar.className = "nav-sticked d-flex flex-md-column"
+}
+const carolChanger = () => {
     rollButton.addEventListener("click", (e) => {
         carolsHidder()
-        let randomizedCarolNumber = carolRandomizer()
+        const randomizedCarolNumber = carolRandomizer()
         console.log("Numer wylosowanej kolędy: " + randomizedCarolNumber)
+        lastValues.push(randomizedCarolNumber)
+        console.log("Historia losowania: " + lastValues)
         document.getElementById(`${randomizedCarolNumber}`).style.display = "block";
-        navBar.className = "nav-sticked d-flex flex-md-column"
-        document.changedCarol = randomizedCarolNumber;
-        document.lastValues.push(document.changedCarol)
-        console.log("Historia losowania: " + document.lastValues)
+        navMobileSticker()
         latestCarolShower()
     })
 }
 carolChanger()
-let latestCarolShower = (e) => {
-    if ((document.lastValues[document.lastValues.length - 2]) != undefined) {
-        lastCarolLabel.style.display = "block";
-        return document.querySelector("#last").innerHTML = document.querySelectorAll("h1")[(document.lastValues[document.lastValues.length - 2]) + 1].innerHTML
-    } else {
-        return document.querySelector("#last").innerHTML = ""
-    }
-}
